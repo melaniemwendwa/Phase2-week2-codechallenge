416704aa199a6aee81cef28852e7db49d6a696be
melanie mwendwa <mwendwathuranira@gmail.com>
2025-07-20 18:48:43 +0300

    update with solution for creation of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Overview from "./components/Overview";
+import GoalForm from "./components/GoalForm";
+import DepositForm from "./components/DepositForm";
+import GoalCard from "./components/GoalCard";
 
 function App() {
+  const [goals, setGoals] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/goals")
+      .then(res => res.json())
+      .then(data => setGoals(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="app-title">Smart Goal Planner</h1>
+      <Overview goals={goals} />
+
+      <div className="forms-row">
+        <GoalForm setGoals={setGoals} goals={goals} />
+        <DepositForm goals={goals} setGoals={setGoals} />
+      </div>
+
+      <div className="goals-container">
+        {goals.map(goal => (
+          <GoalCard key={goal.id} goal={goal} setGoals={setGoals} goals={goals} />
+        ))}
+      </div>
     </div>
   );
 }
